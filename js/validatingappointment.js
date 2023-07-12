ce4fc06cc6027c8e6df63fb6642dc3a4324eb9d8
Quetellen <quetellen.bertazo@gmail.com>
2023-07-11 21:03:27 -0700

    add new try with validation

--- js/validatingappointment.js
+++ js/validatingappointment.js
@@ -58,9 +58,68 @@
 
 
 
- function setValue(element, date) {
-    const isoString = date.toISOString();
-    element.value = isoString.substring(0, isoString.indexOf("T") + 6);
-    };
+//  function setValue(element, date) {
+//     const isoString = date.toISOString();
+//     element.value = isoString.substring(0, isoString.indexOf("T") + 6);
+//     };
 
     
+
+//     const email = document.getElementById("mail");
+
+//     meeting-time.addEventListener("input", (event) => {
+//   if (meeting-time.validity.typeMismatch) {
+//     meeting-time.setCustomValidity("I am expecting an email address!");
+//   } else {
+//     meeting-time.setCustomValidity("");
+//   }
+// });
+
+
+const form = document.querySelector("form");
+const meetingTime = document.getElementById("meetingTime");
+const error = email.nextElementSibling;
+
+// As per the HTML Specification
+const meetingTimeRegExp = "^(sun|Sun|mon|Mon|t(ues|hurs)|(T(ues|hurs))|Fri|fri)(day|\\.)"
+                        + "?$|wed(\\.|nesday)?$|Wed(\\.|nesday)?$|Sat(\\.|urday)"
+                        + "?$|sat(\\.|urday)?$|t((ue?)|(hu?r?))\\.?$|T((ue?)|(hu?r?))\\.?$";
+  
+
+// Now we can rebuild our validation constraint
+// Because we do not rely on CSS pseudo-class, we have to
+// explicitly set the valid/invalid class on our email field
+window.addEventListener("load", () => {
+  // Here, we test if the field is empty (remember, the field is not required)
+  // If it is not, we check if its content is a well-formed email address.
+  const isValid = meetingTime.value.length === 0 || meetingTimeRegExp.test(email.value);
+  meetingTime.className = isValid ? "valid" : "invalid";
+});
+
+// This defines what happens when the user types in the field
+meetingTime.addEventListener("input", () => {
+  const isValid = meetingTime.value.length === 0 || meetingTimeRegExp.test(meetingTime.value);
+  if (isValid) {
+    meetingTime.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+  } else {
+    meetingTime.className = "invalid";
+  }
+});
+
+// This defines what happens when the user tries to submit the data
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const isValid = meetingTime.value.length === 0 || emailRegExp.test(meetingTime.value);
+  if (!isValid) {
+    meetingTime.className = "invalid";
+    error.textContent = "I expect an email, darling!";
+    error.className = "error active";
+  } else {
+    meetingTime.className = "valid";
+    error.textContent = "";
+    error.className = "error";
+  }
+});
